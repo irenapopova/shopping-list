fa97da7957b22d2f014fae1f839541341def3066
irenapopova <irenejpopova6@gmail.com>
2019-10-20 11:45:06 +0200

    fixing shopping cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,8 @@ import { Link } from 'react-router-dom'
                     
                     <ul className="right">
                         <li><Link to="/">Shop</Link></li>
-                        <li><Link to="/cart">My cart</Link></li>
-                        <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
+                        <li><Link to="/cart">My list</Link></li>
+                        <li><Link to="/cart"><i className="material-icons">shopping_list</i></Link></li>
                     </ul>
                 </div>
             </nav>
